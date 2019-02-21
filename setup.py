72ef462081528a7294a13f6e0da640bef809aecc
khuxkm fbexl <khuxkm@ttm.sh>
2019-02-21 03:28:03 -0500

    Make testing python3 only

--- setup.py
+++ setup.py
@@ -39,11 +39,8 @@ setup(
         'Intended Audience :: Developers',
         'License :: WTFPL',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.6',
     ],
     test_suite='tests',
 )
