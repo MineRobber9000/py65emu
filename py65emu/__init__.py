b9a7ef07d08945745447070f09f2697d473bb101
khuxkm fbexl <khuxkm@ttm.sh>
2019-03-01 15:38:36 -0500

    Allow importing from py65emu instead of py65emu.whatever

--- py65emu/__init__.py
+++ py65emu/__init__.py
@@ -4,3 +4,7 @@
 __author__ = 'Robert Miles'
 __email__ = 'khuxkm@tilde.team'
 __version__ = '0.0.0'
+
+from py65emu.cpu import CPU
+from py65emu.mmu import MMU, Block
+from py65emu.mmio import MMIORegister, MMIOBlock
